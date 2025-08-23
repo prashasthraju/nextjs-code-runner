cf4f24e055b976ccb86e1c10613803dbd7c68876
prashasthraju <2023uma0209@iitjammu.ac.in>
2025-08-24 02:49:49 +0530

    changed the cpp filea and the cpp file

--- new.cpp
+++ new.cpp
@@ -1,10 +1,10 @@
-#include <bits/stdtr1c++.h>
-using namespace std;
-int main(int arg,char* argv []){
-    float fl=2.2434f;
-    int flint=(int)fl;
-    int fl2=int (fl);
-    int fl3=static_cast<int>(fl);
+// #include <bits/stdtr1c++.h>
+// using namespace std;
+// int main(int arg,char* argv []){
+//     float fl=2.2434f;
+//     int flint=(int)fl;
+//     int fl2=int (fl);
+//     int fl3=static_cast<int>(fl);
     
-    return 0;
-}
+//     return 0;
+// }
